e75812821a85ac2d89234c7ed8f3c0ac0e600b6f
ArthurHlt <arthurh.halet@gmail.com>
2015-09-12 05:00:21 +0200

    Ajout de deux nouveaux beans
    CompteForm est un bean à utiliser pour l'inscription d'un utilisateur
    CompteSession est la session utilisateur.

---
+++ web/src/main/java/com/MPGI/ECOMSport/beans/CompteSession.java
@@ -0,0 +1,11 @@
+package com.MPGI.ECOMSport.beans;
+
+import com.MPGI.ECOMSport.EJBentity.Compte;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "compteSession")
+@SessionScoped
+public class CompteSession extends Compte {
+}
