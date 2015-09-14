899c46793655363cd76d6d553a3ffb8159141c83
mengolij <mengoli.jeanluc@gmail.com>
2015-09-14 21:20:16 +0200

    save pour pull

---
+++ web/src/main/java/com/MPGI/ECOMSport/beans/Panier.java
@@ -0,0 +1,15 @@
+package com.MPGI.ECOMSport.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.MPGI.ECOMSport.EJBentity.Commande;
+
+
+
+@ManagedBean(name = "compteSession")
+@SessionScoped
+public class Panier extends Commande {
+	
+
+}
